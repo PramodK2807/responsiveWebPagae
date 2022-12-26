a3e538de699b4d2421cb1d9d2858af0cef5303d4
PramodK2808 <pramodkushwaha1328@gmail.com>
2022-12-26 15:25:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+    if (elem.length > 1) {
+      for (let i = 0; i < elem.length; i++) {
+        elem[i].addEventListener(type, callback);
+      }
+    } else {
+      elem.addEventListener(type, callback);
+    }
+  }
+  
+  
+  
+  /**
+   * navbar toggle
+   */
+  
+  const navbar = document.querySelector("[data-navbar]");
+  const navToggler = document.querySelectorAll("[data-nav-toggler]");
+  const overlay = document.querySelector("[data-overlay]");
+  
+  const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  }
+  
+  addEventOnElem(navToggler, "click", toggleNavbar);
+  
+  
+  
+  /**
+   * close navbar when click on any navbar links
+   */
+  
+  const navLinks = document.querySelectorAll("[data-nav-link]");
+  
+  const closeNavbar = function () {
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+  }
+  
+  addEventOnElem(navLinks, "click", closeNavbar);
+  
+  
+  
+  /**
+   * header active when scroll down
+   */
+  
+  // const header = document.querySelector("[data-header]");
+  
+  // const headerActive = function () {
+  //   window.scrollY > 100 ? header.classList.add(".nav-active")
+  //     : header.classList.remove(".nav-active");
+  // }
+  
+  // addEventOnElem(window, "scroll", headerActive);
+  
+
+  const navbarEl = document.querySelector("header");
+  window.addEventListener("scroll", () => {
+    if(window.scrollY > 90) {
+        navbarEl.classList.add("nav-active");
+    }
+    else{
+        navbarEl.classList.remove("nav-active");
+    }
+});
+  
+
+  
+  /**
+   * accordion toggle
+   */
+  
+  const accordionAction = document.querySelectorAll("[data-accordion-action]");
+  
+  const toggleAccordion = function () { this.classList.toggle("active"); }
+  
+  addEventOnElem(accordionAction, "click", toggleAccordion);s
